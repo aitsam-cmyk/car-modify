6aeab0b1203e3723e904c01483034d285169d9a4
SYED AITSAM Shah <aitsamalis@gmail.com>
2026-01-03 11:06:49 +0500

    Initial commit

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const app = express();
+
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
-const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Server is running ✅');
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+
 });
 
 
